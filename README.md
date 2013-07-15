7d250eba0207f09074b3b7c60cbdf2663257e1c0
Jared De Blander <jared@deblander.org>
2013-07-15 00:47:27 +0000

    Initial commit of Jog

---
+++ README.md
@@ -0,0 +1,5 @@
+# Jog
+## Jared's Python Blog
+### Powered by Flask
+***
+This is my simple blog project to learn more about Flask.
---
+++ jog.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env pypy
+#
+# Copyright (c) 2013 Jared De Blander
+# jared0x90@gmail.com
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import url_for
+
+##############################################################################
+# initiate flask
+##############################################################################
+
+app = Flask(__name__, static_folder='static', static_url_path='/static')
+
+##############################################################################
+# Create routes
+##############################################################################
+# General routes
+##############################################################################
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+##############################################################################
+# Run application
+##############################################################################
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 80)
