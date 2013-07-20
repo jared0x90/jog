c372c3404be32f90253ebffb5a12235f676cbdd7
Jared De Blander <jared@deblander.org>
2013-07-20 23:28:04 +0000

    Layout code

--- jog.py
+++ jog.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env pypy
 
 # flask imports
-from flask import g
+from flask import flash
 from flask import Flask
+from flask import g
 from flask import Markup
+from flask import redirect
 from flask import render_template
 from flask import request
+from flask import session
 from flask import url_for
 
 # markdown imports
@@ -25,14 +28,19 @@ USERNAME = 'admin'
 PASSWORD = 'default'
 
 # initiate flask
-app = Flask(__name__, static_folder='static', static_url_path='/static')
+app = Flask(
+    __name__,
+    static_folder='static',
+    static_url_path='/static'
+)
 app.config.from_object(__name__)
 app.config.from_envvar('JOG_SETTINGS', silent=True)
 
-# check for the database
+# check that the database exists
 if not os.path.isfile(app.config['DATABASE']):
     sys.exit('Database: %s not found' % app.config['DATABASE'])
 
+# database routines
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
@@ -60,6 +68,16 @@ def index():
         for row in cur.fetchall()
     ]
     return render_template('index.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
 
 # run application
 if __name__ == "__main__":
